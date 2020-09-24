590bf613a677a148aeeca1e55e9a710be2aadef8
Kevin Gutierrez <rickev20@gmaill.com>
2020-09-23 20:54:06 -0500

    Add project files.

---
+++ Sum200/FormMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Sum200
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblHalfway = new System.Windows.Forms.Label();
+            this.txtHalfway = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblHalfway
+            // 
+            this.lblHalfway.AutoSize = true;
+            this.lblHalfway.Location = new System.Drawing.Point(124, 75);
+            this.lblHalfway.Name = "lblHalfway";
+            this.lblHalfway.Size = new System.Drawing.Size(93, 17);
+            this.lblHalfway.TabIndex = 1;
+            this.lblHalfway.Text = "Halfway Total";
+            // 
+            // txtHalfway
+            // 
+            this.txtHalfway.Location = new System.Drawing.Point(12, 72);
+            this.txtHalfway.Name = "txtHalfway";
+            this.txtHalfway.ReadOnly = true;
+            this.txtHalfway.Size = new System.Drawing.Size(89, 22);
+            this.txtHalfway.TabIndex = 2;
+            this.txtHalfway.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(12, 111);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.ReadOnly = true;
+            this.txtSum.Size = new System.Drawing.Size(89, 22);
+            this.txtSum.TabIndex = 4;
+            this.txtSum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(124, 114);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(36, 17);
+            this.lblSum.TabIndex = 3;
+            this.lblSum.Text = "Sum";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 259);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.txtHalfway);
+            this.Controls.Add(this.lblHalfway);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMain";
+            this.Text = "Sum 200";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblHalfway;
+        private System.Windows.Forms.TextBox txtHalfway;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ Sum200/FormMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sum200
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            
+            for (int i = 0; i <= 200; i++)
+            {
+                sum += i;
+                if (i == 100) 
+                {
+                    txtHalfway.Text = Convert.ToString(sum);
+                }
+                //Console.WriteLine("i = " + i + " sum= " + sum);
+
+            }
+            txtSum.Text = Convert.ToString(sum);
+
+        }
+    }
+}
